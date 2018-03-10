abd3f6774da3aee32b65b9f4d2eb14824da90b69
Mi Hwangbo <mhwangbo@e1z2r5p3.42.us.org>
2018-03-10 14:45:47 -0800

    Draft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,35 +6,36 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/04 23:12:46 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/03/10 13:28:44 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/03/10 14:45:34 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char	*ft_strtrim(const char *s)
 {
-	char	*str;
 	int		i;
 	int		j;
-	int		len;
+	char	*new;
 
-	i = 0;
-	j = 0;
 	if (!s)
 		return (NULL);
-	len = ft_strlen(s) - 1;
-	if (len == -1)
+	i = 0;
+	while (*s == ' ' || *s == '\n' || *s == '\t')
+		s++;
+	i = ft_strlen(s) - 1;
+	if (i == -1)
 		return (ft_strnew(0));
-	str = ft_strnew(len + 2);
-	if (!str)
+	while (!s[i] || s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i--;
+	new = ft_strnew(++i);
+	if (!new)
 		return (NULL);
-	while ((s[i] == ' ' || s[i] == '\n' || s[i] == '\t') && (s[i] != '\0'))
+	j = i;
+	i = 0;
+	while (i < j)
+	{
+		new[i] = s[i];
 		i++;
-	while (s[len] == ' ' || s[len] == '\n' || s[len] == '\t')
-		len--;
-	while (i < (len + 1))
-		str[j++] = s[i++];
-	str[j] = '\0';
-	return (str);
-}
+	}
+	return (new);
