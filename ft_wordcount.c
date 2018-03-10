45a3d57492ba09daa74c0c7748cb7623c2a521c7
Mi Hwangbo <mhwangbo@e1z2r5p3.42.us.org>
2018-03-10 15:15:46 -0800

    Draft

--- ft_wordcount.c
+++ ft_wordcount.c
@@ -6,26 +6,36 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/25 23:42:20 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/03/10 15:13:46 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/03/10 15:15:39 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_wordcount(char const *s, char c)
+int		ft_wordcount(char const *str, char c)
 {
+	int word_count;
 	int i;
-	int j;
+	int has_word;
 
-	i = 0;
-	j = 0;
-	if (!s || !c)
+	if (!str || !c)
 		return (0);
-	while (s[i] != '\0')
+	i = 0;
+	word_count = 0;
+	while (str[i] != '\0')
 	{
-		if ((s[i - 1] == '\0' || s[i - 1] == c) && (s[i] != c))
-			j++;
-		i++;
+		has_word = 0;
+		while (c == str[i] && str[i])
+			i++;
+		while (c != str[i] && str[i])
+		{
+			has_word = 1;
+			i++;
+		}
+		while (c == str[i] && str[i])
+			i++;
+		if (has_word == 1)
+			word_count++;
 	}
-	return (j);
+	return (word_count);
 }
