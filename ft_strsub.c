51918f62b8ddb4058a4d8f3799155645b8c7cae2
Mi Hwangbo <mhwangbo@e1z2r5p3.42.us.org>
2018-03-10 15:11:22 -0800

    Draft

--- ft_strsub.c
+++ ft_strsub.c
@@ -3,42 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strsub.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: myoung <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/24 18:22:32 by myoung            #+#    #+#             */
-/*   Updated: 2016/12/22 07:04:52 by myoung           ###   ########.fr       */
+/*   Created: 2018/03/04 23:10:35 by mhwangbo          #+#    #+#             */
+/*   Updated: 2018/03/04 23:10:41 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-** if the string is null return null
-** make a new string the size of len, with ft_stnew
-** check and make sure it worked.
-** i = 0
-** iterate while i < len
-**  set the postition i in new to the start + i of the str
-**  itterate i
-**
-**  return the new str you made
-*/
-
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char			*new;
-	unsigned int	i;
+	char	*str;
+	size_t	i;
 
-	if (!s)
-		return (NULL);
-	new = ft_strnew(len);
 	i = 0;
-	if (!new)
+	str = (char*)malloc(sizeof(char) * len + 1);
+	if (!s || !str)
 		return (NULL);
 	while (i < len)
 	{
-		new[i] = s[start + i];
+		str[i] = s[start + i];
 		i++;
 	}
-	return (new);
+	str[i] = '\0';
+	return (str);
 }
