59f1eae3ae0e73c82296d6a9e2f6bf19a5bdf61b
Mi Hwangbo <mhwangbo@e1z2r5p3.42.us.org>
2018-03-10 15:16:58 -0800

    Draft

--- ft_wordlen.c
+++ ft_wordlen.c
@@ -6,18 +6,22 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/25 23:42:51 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/03/04 22:25:11 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/03/10 15:16:53 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_wordlen(char const *s, char c)
+int		ft_wordlen(char const *str, char c)
 {
-	int		j;
+	int i;
 
-	j = 0;
-	while (s[j] != c)
-		j++;
-	return (j);
+	if (!str || !c)
+		return (0);
+	i = 0;
+	while (c == str[i] && str[i] != '\0')
+		str++;
+	while (c != str[i] && str[i] != '\0')
+		i++;
+	return (i);
 }
